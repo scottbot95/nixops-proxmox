480eee5b1276ab9319be293b5bb80e558488d547
Scott Techau <scott.techau@gmail.com>
2022-09-02 20:44:25 -0700

    Add flake support

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils}: 
+        flake-utils.lib.eachDefaultSystem (system:
+            let pkgs = nixpkgs.legacyPackages.${system}; in
+            {
+                packages = rec {
+                    nixops-proxmox = import ./default.nix { inherit pkgs; };
+                    default = nixops-proxmox;
+                };
+            }
+        );
+}
